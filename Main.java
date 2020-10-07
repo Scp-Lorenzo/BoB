a32c4edd8bb6573e527c5c7317590241ab8b7134
scappaticci <lorenzoscappaticci12@gmail.com>
2020-10-07 13:08:41 +0200

    V2

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        char [] [] terrain = new char [10] [11];
         int i = 0;
         int a = 0;
-        char [] [] terrain = new char [10] [11];
-
+        int x1 = 5;
+        int y1= 5;
+        int x2 = 3;
+        int y2 = 5;
+        char move;
+        Scanner sc = new Scanner(System.in);
 
         for (a=0; a<11; a++){
             for (i = 0; i<10; i++) {
@@ -16,8 +22,13 @@ public class Main {
             }
             i=0;
             }
-        terrain [5] [5] = 'B';
+
+        terrain [x1] [y1] = 'B';
         terrain [3] [5] = 'R';
+
+
+
+do{
 
 
 for (a=0; a<11; a++){
@@ -29,7 +40,45 @@ for (a=0; a<11; a++){
     i=0;
 }
 
-terrain [5] [5] = 1;
+
+
+System.out.println("direction (z q s d) : ");
+move = sc.next().charAt(0);
+
+
+if (move == 's'){
+    y1 = y1 + 1;
+    }
+
+if (move == 'z'){
+        y1 = y1 - 1;
+    }
+
+if (move == 'd'){
+    x1 = x1 + 1;
+    }
+
+if (move == 'q'){
+    x1 = x1 - 1;
+    }
+
+
+
+    for (a=0; a<11; a++){
+        for (i = 0; i<10; i++) {
+            terrain [i] [a] = 'O';
+
+        }
+        i=0;
+    }
+
+
+    terrain [x1] [y1] = 'B';
+    terrain [x2] [y2] = 'R';
+}
+while(a != i);
 
     }
 }
+
+
