2649a7fff01e16a7a7b289b5c7ffb6c85eb6166a
scappaticci <lorenzoscappaticci12@gmail.com>
2020-10-07 13:31:09 +0200

    V3

--- Main.java
+++ Main.java
@@ -12,6 +12,9 @@ public class Main {
         int y1= 5;
         int x2 = 3;
         int y2 = 5;
+        int xb1, yb1, xb2, yb2;
+
+
         char move;
         Scanner sc = new Scanner(System.in);
 
@@ -24,9 +27,7 @@ public class Main {
             }
 
         terrain [x1] [y1] = 'B';
-        terrain [3] [5] = 'R';
-
-
+        terrain [x2] [y2] = 'R';
 
 do{
 
@@ -39,10 +40,11 @@ for (a=0; a<11; a++){
     System.out.println("");
     i=0;
 }
+xb1 = x1;
+yb1 = y1;
 
 
-
-System.out.println("direction (z q s d) : ");
+System.out.println("joueur 1 direction (z q s d) : ");
 move = sc.next().charAt(0);
 
 
@@ -61,20 +63,33 @@ if (move == 'd'){
 if (move == 'q'){
     x1 = x1 - 1;
     }
+xb2 = x2;
+yb2 = y2;
+
+    System.out.println("joueur 2 direction (z q s d) : ");
+    move = sc.next().charAt(0);
 
 
-
-    for (a=0; a<11; a++){
-        for (i = 0; i<10; i++) {
-            terrain [i] [a] = 'O';
-
-        }
-        i=0;
+    if (move == 's'){
+        y2 = y2 + 1;
     }
 
+    if (move == 'z'){
+        y2 = y2 - 1;
+    }
+
+    if (move == 'd'){
+        x2 = x2 + 1;
+    }
+
+    if (move == 'q'){
+        x2 = x2 - 1;
+    }
 
     terrain [x1] [y1] = 'B';
     terrain [x2] [y2] = 'R';
+    terrain [xb1] [yb1] = 'x';
+    terrain [xb2] [yb2] ='x';
 }
 while(a != i);
 
